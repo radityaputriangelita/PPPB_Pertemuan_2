c72f64521b6e251efaa150470ea731925b709d4c
radityaputriangelita <radityaputriangelita@mail.ugm.ac.id>
2023-08-24 22:09:58 +0700

    first update

--- app/src/main/java/com/example/pppb_pertemuan_2/MainActivity.kt
+++ app/src/main/java/com/example/pppb_pertemuan_2/MainActivity.kt
@@ -7,7 +7,6 @@ import com.example.pppb_pertemuan_2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var number = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         var binding = ActivityMainBinding.inflate(layoutInflater)
